a979378360d6930b565c69920a9daaaff7374b80
CSigelmann <sigelman16@up.edu>
2015-10-10 15:03:16 -0700

    Added barracks/marine building and improved supply depot logic

--- src/TestBot1.java
+++ src/TestBot1.java
@@ -44,53 +44,66 @@ public class TestBot1 extends DefaultBWListener {
         for (Unit myUnit : self.getUnits()) {
             units.append(myUnit.getType()).append(" ").append(myUnit.getTilePosition()).append("\n");
 
-            //if there's enough minerals, train an SCV
-            if (myUnit.getType() == UnitType.Terran_Command_Center && self.minerals() >= 50) {
+            //if there's enough minerals, and not currently training an SCV, train an SCV
+            if (myUnit.getType() == UnitType.Terran_Command_Center && self.minerals() >= 50 && !myUnit.isTraining()) {
                 myUnit.train(UnitType.Terran_SCV);
             }
 
-            //if it's a drone and it's idle, send it to the closest mineral patch
+            //if it's a drone and it's idle, decide what to do
             if (myUnit.getType().isWorker() && myUnit.isIdle()) {
-                Unit closestMineral = null;
-
-                //find the closest mineral
-                for (Unit neutralUnit : game.neutral().getUnits()) {
-                    if (neutralUnit.getType().isMineralField()) {
-                        if (closestMineral == null || myUnit.getDistance(neutralUnit) < myUnit.getDistance(closestMineral)) {
-                            closestMineral = neutralUnit;
-                        }
-                    }
-                }
-
-                //if a mineral patch was found, send the drone to gather it
-                if (closestMineral != null) {
-                    myUnit.gather(closestMineral, false);
-                }
-            }
-        }
-
-        //if there's enough minerals, build a supply depot
-        if ((self.minerals() >= 100) && (self.supplyTotal()-self.supplyUsed() <= 3)) 
-        {
-        	//find a worker
-        	for (Unit myUnit : self.getUnits())
-        	{
-        		if(myUnit.getType().isWorker())
-        		{
+                
+            	//if there's enough minerals and we need supply, build supply depot
+                if ((self.minerals() >= 100) && (self.supplyTotal()-self.supplyUsed() <= 2)) 
+                {
         			// find a place to build a supply depot
         			TilePosition buildTile = getBuildTile(myUnit, UnitType.Terran_Supply_Depot, self.getStartLocation());
         			// if found, send worker to build it
         			if (buildTile != null)
         			{
         				myUnit.build(buildTile, UnitType.Terran_Supply_Depot);
-        				break;
         			}
-        		}
-        	}
+                }
+                //if there's enough minerals and we don't have too many, build a barracks
+                else if(self.minerals() >= 150 && self.allUnitCount(UnitType.Terran_Barracks) + self.incompleteUnitCount(UnitType.Terran_Barracks) <= 2)
+                {
+                	TilePosition buildTile = getBuildTile(myUnit, UnitType.Terran_Barracks, self.getStartLocation());
+                	if(buildTile != null)
+                	{
+                		myUnit.build(buildTile, UnitType.Terran_Barracks);
+                	}
+                }
+                else
+            	{
+	            	Unit closestMineral = null;
+	            	
+	
+	                //find the closest mineral
+	                for (Unit neutralUnit : game.neutral().getUnits()) {
+	                    if (neutralUnit.getType().isMineralField()) {
+	                        if (closestMineral == null || myUnit.getDistance(neutralUnit) < myUnit.getDistance(closestMineral)) {
+	                            closestMineral = neutralUnit;
+	                        }
+	                    }
+	                }
+	
+	                //if a mineral patch was found, send the drone to gather it
+	                if (closestMineral != null) {
+	                    myUnit.gather(closestMineral, false);
+	                }
+            	}
+            }
+            
+            // make the barracks build marines
+            if(myUnit.getType() == UnitType.Terran_Barracks && self.minerals() >= 50 && !myUnit.isTraining())
+            {
+            	myUnit.train(UnitType.Terran_Marine);
+            }
         }
+
+        
         
         //draw my units on screen
-        game.drawTextScreen(10, 25, units.toString());
+        //game.drawTextScreen(10, 25, units.toString());
     }
     
     public TilePosition getBuildTile(Unit builder, UnitType buildingType, TilePosition aroundTile)
