73511efdd693203eda2756522d237303b56ed877
Max Robinson <maxrgit@gmail.com>
2015-10-22 20:28:04 -0700

    Now Can Scout
    Now at the very start of the game, I send an SCV to scout the opponent.
    It will check all start locations then return home.
    Supposedly it saves the base locations as well.

--- src/TestBot1.java
+++ src/TestBot1.java
@@ -1,5 +1,8 @@
+import java.util.*;
+
 import bwapi.*;
 import bwta.BWTA;
+import bwta.BaseLocation;
 
 public class TestBot1 extends DefaultBWListener {
 
@@ -8,7 +11,10 @@ public class TestBot1 extends DefaultBWListener {
     private Game game;
 
     private Player self;
-
+    
+    private Unit scout = null;
+    private HashSet<Position> enemyBuildingMemory = new HashSet<Position>();
+    
     public void run() {
         mirror.getModule().setEventListener(this);
         mirror.startGame();
@@ -37,75 +43,19 @@ public class TestBot1 extends DefaultBWListener {
     public void onFrame() {
         game.setTextSize(10);
         game.drawTextScreen(10, 10, "Playing as " + self.getName() + " - " + self.getRace());
-
-        StringBuilder units = new StringBuilder("My units:\n");
-
-        //iterate through my units
-        for (Unit myUnit : self.getUnits()) {
-            units.append(myUnit.getType()).append(" ").append(myUnit.getTilePosition()).append("\n");
-
-            //if there's enough minerals, and not currently training an SCV, train an SCV
-            if (myUnit.getType() == UnitType.Terran_Command_Center && self.minerals() >= 50 && !myUnit.isTraining()) {
-                myUnit.train(UnitType.Terran_SCV);
-            }
-
-            //if it's a drone and it's idle, decide what to do
-            if (myUnit.getType().isWorker() && myUnit.isIdle()) {
-                
-            	//if there's enough minerals and we need supply, build supply depot
-                if ((self.minerals() >= 100) && (self.supplyTotal()-self.supplyUsed() <= 2)) 
-                {
-        			// find a place to build a supply depot
-        			TilePosition buildTile = getBuildTile(myUnit, UnitType.Terran_Supply_Depot, self.getStartLocation());
-        			// if found, send worker to build it
-        			if (buildTile != null)
-        			{
-        				myUnit.build(buildTile, UnitType.Terran_Supply_Depot);
-        			}
-                }
-                //if there's enough minerals and we don't have too many, build a barracks
-                else if(self.minerals() >= 150 && self.allUnitCount(UnitType.Terran_Barracks) + self.incompleteUnitCount(UnitType.Terran_Barracks) <= 2)
-                {
-                	TilePosition buildTile = getBuildTile(myUnit, UnitType.Terran_Barracks, self.getStartLocation());
-                	if(buildTile != null)
-                	{
-                		myUnit.build(buildTile, UnitType.Terran_Barracks);
-                	}
-                }
-                else
-            	{
-	            	Unit closestMineral = null;
-	            	
-	
-	                //find the closest mineral
-	                for (Unit neutralUnit : game.neutral().getUnits()) {
-	                    if (neutralUnit.getType().isMineralField()) {
-	                        if (closestMineral == null || myUnit.getDistance(neutralUnit) < myUnit.getDistance(closestMineral)) {
-	                            closestMineral = neutralUnit;
-	                        }
-	                    }
-	                }
-	
-	                //if a mineral patch was found, send the drone to gather it
-	                if (closestMineral != null) {
-	                    myUnit.gather(closestMineral, false);
-	                }
-            	}
-            }
-            
-            // make the barracks build marines
-            if(myUnit.getType() == UnitType.Terran_Barracks && self.minerals() >= 50 && !myUnit.isTraining())
-            {
-            	myUnit.train(UnitType.Terran_Marine);
-            }
-        }
-
         
-        
-        //draw my units on screen
-        //game.drawTextScreen(10, 25, units.toString());
+        manageUnits();
+        manageEnemyBuildings();
+
     }
     
+    /**
+     * 
+     * @param builder
+     * @param buildingType
+     * @param aroundTile
+     * @return
+     */
     public TilePosition getBuildTile(Unit builder, UnitType buildingType, TilePosition aroundTile)
     {
     	TilePosition ret = null;
@@ -144,6 +94,135 @@ public class TestBot1 extends DefaultBWListener {
     	
     	if(ret == null) game.printf("Unable to find suitable build position for "+buildingType.toString());
     	return ret;
+    }
+    
+    /**
+     * Manage Units
+     * 
+     */
+    public void manageUnits(){
+    	StringBuilder units = new StringBuilder("My units:\n");
+    	//iterate through my units
+        for (Unit myUnit : self.getUnits()) {
+            units.append(myUnit.getType()).append(" ").append(myUnit.getTilePosition()).append("\n");
+
+            //if there's enough minerals, and not currently training an SCV, train an SCV
+            if (myUnit.getType() == UnitType.Terran_Command_Center && self.minerals() >= 50 && !myUnit.isTraining()) {
+                myUnit.train(UnitType.Terran_SCV);
+            }
+
+            //if it's a drone and it's idle, decide what to do
+            if (myUnit.getType().isWorker() && myUnit.isIdle()) {
+            	
+            	//Scout
+            	if(scout == null && game.enemy().getUnits().size() == 0){
+            		scout(myUnit);
+            		continue;
+            	}
+                
+            	//if there's enough minerals and we need supply, build supply depot
+                if ((self.minerals() >= 100) && (self.supplyTotal()-self.supplyUsed() <= 2)) 
+                {
+        			// find a place to build a supply depot
+        			TilePosition buildTile = getBuildTile(myUnit, UnitType.Terran_Supply_Depot, self.getStartLocation());
+        			// if found, send worker to build it
+        			if (buildTile != null)
+        			{
+        				myUnit.build(buildTile, UnitType.Terran_Supply_Depot);
+        			}
+                }
+                //if there's enough minerals and we don't have too many, build a barracks
+                else if(self.minerals() >= 150 && self.allUnitCount(UnitType.Terran_Barracks) + self.incompleteUnitCount(UnitType.Terran_Barracks) <= 2)
+                {
+                	TilePosition buildTile = getBuildTile(myUnit, UnitType.Terran_Barracks, self.getStartLocation());
+                	if(buildTile != null)
+                	{
+                		myUnit.build(buildTile, UnitType.Terran_Barracks);
+                	}
+                }
+                else
+            	{
+	            	Unit closestMineral = null;
+	            	
+	
+	                //find the closest mineral
+	                for (Unit neutralUnit : game.neutral().getUnits()) {
+	                    if (neutralUnit.getType().isMineralField()) {
+	                        if (closestMineral == null || myUnit.getDistance(neutralUnit) < myUnit.getDistance(closestMineral)) {
+	                            closestMineral = neutralUnit;
+	                        }
+	                    }
+	                }
+	            			
+	                //if a mineral patch was found, send the drone to gather it
+	                if (closestMineral != null) {
+	                    myUnit.gather(closestMineral, false);
+	                }
+            	}
+            }
+            
+            // make the barracks build marines
+            if(myUnit.getType() == UnitType.Terran_Barracks && self.minerals() >= 50 && !myUnit.isTraining())
+            {
+            	myUnit.train(UnitType.Terran_Marine);
+            }
+        }
+        //draw my units on screen
+//        game.drawTextScreen(10, 25, units.toString());
+    }
+    
+    public void scout(Unit unit){
+    	scout = unit;
+    	List<BaseLocation> baseLocations = BWTA.getBaseLocations();
+    	for(BaseLocation base : baseLocations){
+    		TilePosition baseToP = new TilePosition(base.getX()/32, base.getY()/32);
+    		if (base.isStartLocation() && baseToP != self.getStartLocation()){
+    			if(unit.isIdle()){
+    				unit.move(base.getPosition());
+    			}
+    			else{
+    				unit.move(base.getPosition(), true);
+    			}
+    		}
+    	}
+    	//Add home as the last place to go
+    	Position homePosition = new Position(self.getStartLocation().getX(), self.getStartLocation().getY());
+    	unit.move(homePosition, true);	
+    }
+    
+    public void manageEnemyBuildings(){
+    	//Add any buildings we see to list.
+    	for(Unit u: game.enemy().getUnits()){
+    		//if this unit is a building add it to the hash
+    		if(u.getType().isBuilding()){
+    			//check if we have it's position in memory and add it if we don't
+    			if(!enemyBuildingMemory.contains(u.getPosition())){
+    				enemyBuildingMemory.add(u.getPosition());
+    			}
+    		}
+    	}
+    	
+    	//loop over the visible enemy units that we remember
+    	for(Position p : enemyBuildingMemory){
+    		TilePosition tileCorrespondingToP = new TilePosition(p.getX()/32, p.getY()/32);
+    		
+    		//if visible
+    		if(game.isVisible(tileCorrespondingToP)){
+    			//loop over the visible enemy buildings and find out if at least
+    			// one of them is still at the remembered position
+    			boolean buildingStillThere = false;
+    			for(Unit u: game.enemy().getUnits()){
+    				if(u.getType().isBuilding() && u.getPosition() == p){
+    					buildingStillThere = true;
+    					break;
+    				}
+    			}
+    			if(!buildingStillThere){
+    				enemyBuildingMemory.remove(p);
+    				break;
+    			}
+    		}
+    	}
     }
 
     public static void main(String[] args) {
