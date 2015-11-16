839f38a510c927195a50dd45b5ea056b4631cefb
CSigelmann <sigelman16@up.edu>
2015-11-16 00:18:02 -0800

    Created ArmyManager class with stub methods.

---
+++ src/ArmyManager.java
@@ -0,0 +1,42 @@
+import bwapi.*;
+
+/**
+ * ArmyManager Class
+ * 	This class is responsible for handling all direct orders to
+ * 	military units when they are not in combat situations.
+ * 
+ * @author Casey Sigelmann
+ *
+ */
+public class ArmyManager {
+
+	/**
+	 * defend()
+	 * Positions the defend squad in a defensive position around our base.
+	 */
+	public void defend()
+	{
+		
+	}
+	
+	/**
+	 * engage()
+	 * Makes the offensive squad attack the given location.
+	 * 
+	 * @param position - the location to attack
+	 */
+	public void engage(Position position)
+	{
+		
+	}
+	
+	/**
+	 * scout()
+	 * Moves units in the scout squad to unexplored locations.
+	 */
+	public void scout()
+	{
+		
+	}
+	
+}
