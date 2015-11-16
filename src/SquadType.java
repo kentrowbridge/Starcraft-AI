7be35dc7c52ae0429763493931ae4aa4c61376a4
CSigelmann <sigelman16@up.edu>
2015-11-16 00:50:04 -0800

    Created SquadType enum.

---
+++ src/SquadType.java
@@ -0,0 +1,13 @@
+/**
+ * SquadType Enum
+ * An enum containing all possible types for squads.
+ * 
+ * @author Casey Sigelmann
+ *
+ */
+public enum SquadType {
+	Offense,
+	Defense,
+	Scout,
+	Reserve;
+}
