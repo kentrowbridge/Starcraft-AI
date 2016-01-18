302ab2243abbac887759362d10558c32390a5f15
abowns <bowns16@up.edu>
2016-01-17 17:44:55 -0800

    Adding formatting edits and some comments to files

--- src/StarcraftAI/Command.java
+++ src/StarcraftAI/Command.java
@@ -2,9 +2,9 @@ package StarcraftAI;
 /**
  * Commands Enum
  * An enum containing all possible commands from the StrategyManager to the MilitaryManager
- *
  */
-public enum Command {
+public enum Command 
+{
 	Attack, 
 	Defend, 
 	Scout;
