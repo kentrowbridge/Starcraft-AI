b5832174483c7e760a04cead380450521c6ad2ae
Kenny Trowbridge <Kenny Trowbridge>
2016-02-24 20:22:55 -0800

    State Transition functional with unit tests

--- src/StarcraftAI/StateTransition.java
+++ src/StarcraftAI/StateTransition.java
@@ -1,41 +1,32 @@
 package StarcraftAI;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Hashtable;
+import java.util.List;
 
 import bwapi.*;
 
 public class StateTransition {
-	public static State transition(State s, UnitType goal) 
+	private static String[] unitsArr = {"Terran_Marine","Terran_Medic","Terran_SCV","Terran_Siege_Tank_Tank_Mode",
+						 "Terran_Vulture","Terran_Academy","Terran_Barracks","Terran_Command_Center",
+						 "Terran_Factory","Terran_Machine_Shop","Terran_Supply_Depot"};
+	private static List<String> validUnits = new ArrayList<String>(Arrays.asList(unitsArr));
+	
+	public static State transition(State s, String goal) 
 	{
 		State newState = new State(s);
 		
-		Hashtable<String, Integer> units = newState.getUnits();
-		String goalString = goal.c_str();
-		int goalCount = units.get(goalString) == null ? 0 : units.get(goalString);  
+		Hashtable<String, Integer> units = newState.getUnits();		
+		int goalCount = units.get(goal) == null ? 0 : units.get(goal);  
 		
-		switch(goalString) 
-		{
-		//units
-		case "Terran_Marine":
-		case "Terran_Medic":
-		case "Terran_SCV":
-		case "Terran_Siege_Tank_Tank_Mode":
-		case "Terran_Vulture":			
-		//buildings
-		case "Terran_Academy":
-		case "Terran_Barracks":
-		case "Terran_Command_Center":
-		case "Terran_Factory":
-		case "Terran_Machine_Shop":
-		case "Terran_Supply_Depot":
-		
-			units.put(goalString, goalCount + 1);
-			break;			
-		default:
-			System.out.println("Goal not available");
-			break;
+		if(validUnits.contains(goal)) {
+			units.put(goal, goalCount + 1);
 		}
-		
+		else {
+			System.out.println("Transition goal not recognized: " + goal);
+		}
+				
 		newState.setUnits(units);
 		return newState;
 	}
--- src/unitTests/StateTransitionTest.java
+++ src/unitTests/StateTransitionTest.java
@@ -2,6 +2,8 @@ package unitTests;
 
 import static org.junit.Assert.*;
 
+import java.util.Hashtable;
+
 import org.junit.Test;
 
 import StarcraftAI.State;
@@ -11,14 +13,58 @@ import bwapi.*;
 public class StateTransitionTest {
 
 	@Test
-	public void test1() {
+	public void simpleUnitTest() {
 		State s = new State();
+		String goal = "Terran_Marine";
 		
-		State newS = StateTransition.transition(s, UnitType.Terran_Marine);
+		State newS = StateTransition.transition(s, goal);
 		
-		Integer oldMarines = s.getUnits().get(UnitType.Terran_Marine.c_str());
-		Integer newMarines = newS.getUnits().get(UnitType.Terran_Marine.c_str());
+		Integer oldMarines = s.getUnits().get(goal) == null ? 0 : s.getUnits().get(goal);
+		Integer newMarines = newS.getUnits().get(goal) == null ? 0 : newS.getUnits().get(goal);
 		assertTrue(oldMarines + 1 == newMarines);
 	}
-
+	
+	@Test
+	public void simpleBuildingTest() {
+		State s = new State();
+		String goal = "Terran_Barracks";
+		
+		State newS = StateTransition.transition(s, goal);
+		
+		Integer oldGoalUnits = s.getUnits().get(goal) == null ? 0 : s.getUnits().get(goal);
+		Integer newGoalUnits = newS.getUnits().get(goal) == null ? 0 : newS.getUnits().get(goal);
+		assertTrue(oldGoalUnits + 1 == newGoalUnits);
+	}
+	
+	@Test
+	public void multipleTransitionsTest() {
+		State s = new State();
+		State newS = new State(s);
+		String[] goals = {"Terran_Barracks","Terran_Marine","Terran_Academy",
+						  "Terran_Medic","Terran_Factory","Terran_Marine"};
+		
+		
+		for(String g : goals) {
+			newS = StateTransition.transition(newS, g);
+		}
+		
+		for(String g : goals) {			
+			Integer oldGoalUnits = s.getUnits().get(g) == null ? 0 : s.getUnits().get(g);
+			Integer newGoalUnits = newS.getUnits().get(g) == null ? 0 : newS.getUnits().get(g);
+			System.out.println(g + ": " + oldGoalUnits + " --> " + newGoalUnits);
+			assertTrue(oldGoalUnits < newGoalUnits);
+		}
+	}
+	
+	@Test
+	public void invalidGoalTest() {
+		State s = new State();
+		String goal = "Marine";
+		
+		State newS = StateTransition.transition(s, goal);
+		
+		Integer oldGoalUnits = s.getUnits().get(goal) == null ? 0 : s.getUnits().get(goal);
+		Integer newGoalUnits = newS.getUnits().get(goal) == null ? 0 : newS.getUnits().get(goal);
+		assertFalse(oldGoalUnits + 1 == newGoalUnits);
+	}
 }
